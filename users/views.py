d596a4d2838688439df7c649fc8bb6e605caa884
nezl0i <nezl0in@gmail.com>
2021-10-18 19:30:33 +0300

    create login, registration form. Refactoring html

--- users/views.py
+++ users/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
-from users.forms import UserLoginForm
+from django.shortcuts import render, HttpResponseRedirect
+from users.forms import UserLoginForm, UserRegistrationForm
+from django.urls import reverse
+from django.contrib import auth
 
 
 def login(request):
-    form = UserLoginForm()
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+        else:
+            print(form.errors)
+
+    else:
+        form = UserLoginForm()
     context = {
         'title': 'GeekShop - Авторизация',
         'form': form,
@@ -12,5 +27,19 @@ def login(request):
 
 
 def registration(request):
-    context = {'title': 'GeekShop - Регистрация'}
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+        else:
+            print(form.errors)
+    else:
+        form = UserRegistrationForm()
+    context = {'title': 'GeekShop - Регистрация', 'form': form}
     return render(request, 'users/registration.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
