628891728a4fa76338170097f26aa2871c3fff97
nezl0i <nezl0in@gmail.com>
2021-10-22 10:02:40 +0300

    refactoring

--- static/js/basket.js
+++ static/js/basket.js
@@ -1,9 +1,8 @@
 window.onload = function () {
     $('.basket_list').on('click', 'input[type="number"]', function () {
         var t_href = event.target;
-        console.log(t_href.name); // basket.id
-        console.log(t_href.value); // basket.quantity
-
+        // console.log(t_href.name); // basket.id
+        // console.log(t_href.value); // basket.quantity
         $.ajax({
             url: '/baskets/edit/' + t_href.name + '/' + t_href.value + '/',
             success: function (data) {
