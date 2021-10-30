199d84c84754a8428706a6412c4dd57fab6f51a6
nezl0i <nezl0in@gmail.com>
2021-10-30 14:14:50 +0300

    added paginator

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ import os
 from datetime import datetime
 from django.shortcuts import render
 from products.models import ProductCategory, Product
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 MODULE_DIR = os.path.dirname(__file__)
 
@@ -14,7 +15,7 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request, category_id=None):
+def products(request, category_id=None, page=1):
     context = {
         'title': 'GeekShop - Каталог',
         'categories': ProductCategory.objects.all(),
@@ -23,5 +24,12 @@ def products(request, category_id=None):
         product = Product.objects.filter(category_id=category_id)
     else:
         product = Product.objects.all()
-    context['products'] = product
+    paginator = Paginator(product, 3)
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+    context['products'] = products_paginator
     return render(request, 'products/products.html', context)
