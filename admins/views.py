3a9530643291dad1816c6d7d6398d6fd2799a08d
nezl0i <nezl0in@gmail.com>
2021-10-30 10:32:02 +0300

    created admins app

---
+++ admins/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+
+def index(request):
+    context = {'title': 'GeekShop - Админ Панель'}
+    return render(request, 'admins/index.html', context)
+
+
+# Create
+def admin_users_create(request):
+    context = {'title': 'GeekShop - Создание пользователей'}
+    return render(request, 'admins/admin-users-create.html', context)
+
+
+# Read
+def admin_users(request):
+    context = {'title': 'GeekShop - Пользователи'}
+    return render(request, 'admins/admin-users-read.html', context)
+
+
+# Update
+def admin_users_update(request):
+    context = {'title': 'GeekShop - Обновление Пользователя'}
+    return render(request, 'admins/admin-users-update-delete.html', context)
+
+
+# Delete
+def admin_users_delete(request):
+    pass
