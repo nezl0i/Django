93a1b98fe676e5384210aa825ccf7c262d413329
nezl0i <nezl0in@gmail.com>
2021-10-30 13:27:36 +0300

    added filter from category

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ app_name = 'products'
 
 urlpatterns = [
     path('', products, name='index'),
+    path('<int:category_id>/', products, name='category'),
 ]
--- products/views.py
+++ products/views.py
@@ -14,12 +14,14 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request):
-    # file_path = os.path.join(MODULE_DIR, 'fixtures/products.json')
+def products(request, category_id=None):
     context = {
         'title': 'GeekShop - Каталог',
-        # 'products': json.load(open(file_path, encoding='utf-8'))
-        'products': Product.objects.all(),
         'categories': ProductCategory.objects.all(),
     }
+    if category_id:
+        product = Product.objects.filter(category_id=category_id)
+    else:
+        product = Product.objects.all()
+    context['products'] = product
     return render(request, 'products/products.html', context)
